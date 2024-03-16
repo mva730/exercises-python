659c380d2513ecb8a1c1e71604cb3c5c5eedc3bf
v.milashov <v.milashov@tinkoff.ru>
2024-03-16 13:38:18 +0300

    median.py

---
+++ exercises/leetcode/median.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        while
+
+
+print(Solution().findMedianSortedArrays([1, 2], [3, 4]))
