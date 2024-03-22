b21a1fa420b87be6184db0d6fd626e78460353f6
v.milashov <v.milashov@tinkoff.ru>
2024-03-22 09:16:51 +0300

    merge_k_lists2.py

---
+++ exercises/hackerrank/strings/alternate.py
@@ -0,0 +1,41 @@
+import itertools
+
+
+def alternate(s):
+    set_of_chars = set(list(s))
+    i = 0
+
+    combos = list(itertools.combinations(set_of_chars, 2))
+
+    container = []
+    for combo in combos:
+        res = ''
+        for ch in s:
+            if ch in combo:
+                res += ch
+        container.append(res)
+
+    for string in container:
+        prev = string[0]
+        for i in range(1, len(string)):
+            if prev == string[i]:
+                container.remove(string)
+                break
+            prev = string[i]
+            i += 1
+
+    return max(container, key=len)
+
+    # strings_container = []
+    # for char_to_remove in set_of_chars:
+    #     result = []
+    #     for ch in list_of_chars:
+    #         if char_to_remove != ch:
+    #             result.append(ch)
+    #
+    #     strings_container.append(''.join(result))
+    #
+    # return strings_container
+
+
+print(alternate("asdcbsdcagfsdbgdfanfghbsfdab"))
